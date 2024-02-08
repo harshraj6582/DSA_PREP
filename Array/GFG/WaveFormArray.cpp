5262fae147eb2a3ce1074d996da3ccb8b4655286
harshraj6582 <harshraj211430@gmail.com>
2024-02-08 11:21:25 +0530

    Wave Form of Matrix

---
+++ Array/GFG/WaveFormArray.cpp
@@ -0,0 +1,30 @@
+// #include<iostream>
+// using namespace std;
+
+// // This is used to print the array in the wave form
+
+// int main() {
+//     int arr[4][4] = {{1,2,3,4},
+//                      {5,6,7,8},
+//                      {9,10,11,12},
+//                      {13,14,15,16}};
+                   
+//     for(int j = 0 ; j < 4 ; j++) {
+//         // This is just used for the loop when the col index is even it will print from 0 to N 
+//         if (j % 2 == 0) {
+//             for(int i = 0 ; i < 4 ; i++) {
+//                 cout << arr[i][j];
+//             }
+//             cout<<endl ; 
+//         }
+//         // This is just used for the loop when the col index is odd it will print from N to 0
+//         else {
+//             for(int i = 3 ; i >= 0 ; i--) {
+//                 cout << arr[i][j]<<" ";
+//             }
+//             cout<<endl ; 
+//         }
+//     }
+            
+//     return 0;
+// }
