003eb0dc1b5f87637de304ed9b39cec2d4c901d0
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-05-01 12:46:54 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (95.66%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,62 +1,67 @@
 #include <string>
-using namespace std;
 
 class Solution {
 public:
-    string intToRoman(int num) {
-        string ans = "";
+    std::string intToRoman(int num) {
+        std::string ans = "";
         while (num > 0) {
             while (num >= 1000) {
                 ans.push_back('M');
-                num -= 1000;
+                num = num - 1000;
             }
             while (num >= 900) {
-                ans += "CM";
-                num -= 900;
+                ans.push_back('C');
+                ans.push_back('M');
+                num = num - 900;
             }
             while (num >= 500) {
                 ans.push_back('D');
-                num -= 500;
+                num = num - 500;
             }
             while (num >= 400) {
-                ans += "CD";
-                num -= 400;
+                ans.push_back('C');
+                ans.push_back('D');
+                num = num - 400;
             }
             while (num >= 100) {
                 ans.push_back('C');
-                num -= 100;
+                num = num - 100;
             }
             while (num >= 90) {
-                ans += "XC";
-                num -= 90;
+                ans.push_back('X');
+                ans.push_back('C');
+                num = num - 90;
             }
             while (num >= 50) {
                 ans.push_back('L');
-                num -= 50;
+                num = num - 50;
             }
             while (num >= 40) {
-                ans += "XL";
-                num -= 40;
+                ans.push_back('X');
+                ans.push_back('L');
+                num = num - 40;
             }
             while (num >= 10) {
                 ans.push_back('X');
-                num -= 10;
+                num = num - 10;
             }
             while (num >= 9) {
-                ans += "IX";
-                num -= 9;
+                ans.push_back('I');
+                ans.push_back('X');
+                num = num - 9;
             }
             while (num >= 5) {
                 ans.push_back('V');
-                num -= 5;
+                num = num - 5;
             }
             while (num >= 4) {
-                ans += "IV";
-                num -= 4;
+                ans.push_back('I');
+                ans.push_back('V');
+                num = num - 4;
             }
             while (num >= 1) {
                 ans.push_back('I');
-                num -= 1;
+                num = num - 1;
             }
         }
         return ans;
