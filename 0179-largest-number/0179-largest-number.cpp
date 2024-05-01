2ebe9ccf8fd28af99e7ff67a91ca2f1f975e973f
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-05-01 17:34:06 +0530

    Time: 8 ms (24.90%), Space: 16 MB (16.18%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,3 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool compareStrings(const string& a, const string& b) {
+    return a + b > b + a;
+}
+
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
@@ -6,15 +17,14 @@ public:
             numsStr.push_back(to_string(num));
         }
         
-        sort(numsStr.begin(), numsStr.end(), [](const string& a, const string& b) {
-            return a + b > b + a;
-        });
+        sort(numsStr.begin(), numsStr.end(), compareStrings);
         
         string largestNum;
         for (const string& numStr : numsStr) {
             largestNum += numStr;
         }
         
+        // Handle the case when the largestNum is "0"
         if (largestNum[0] == '0') {
             return "0";
         }
@@ -22,3 +32,4 @@ public:
         return largestNum;
     }
 };
+
