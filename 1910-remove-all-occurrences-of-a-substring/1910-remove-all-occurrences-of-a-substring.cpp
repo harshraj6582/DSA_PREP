fc4967749b57cef7c1d58a590328e6e258cbd49d
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-06-17 10:59:22 +0530

    Time: 6 ms (23.72%), Space: 7.9 MB (37.77%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        
-        // Each Time Checking if the Possible value is present in the Substring or not 
         while(s.find(part) != string::npos){
             s.erase(s.find(part) , part.length());
         }
