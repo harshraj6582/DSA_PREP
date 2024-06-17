8c5f761127033ee90c186f29d97a6bcaf5bca73c
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-06-17 11:05:16 +0530

    Time: 6 ms (23.72%), Space: 8.5 MB (14.05%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,34 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        while(s.find(part) != string::npos){
-            s.erase(s.find(part) , part.length());
+    
+    string Recursive_Solution(string &s , string part ){
+        // Check for the Base Case
+        if(s.find(part) == string::npos){
+            // That Certain  Part is not present in the String 
+            return  s ; 
         }
-        return s ; 
+        else{
+             // One Case 
+            s.erase(s.find(part) , part.length());
+            // Next Recursive Call
+             return Recursive_Solution(s, part);
+        }
+       
+        
+        
+        
+        
+        
+    }
+    
+    
+    string removeOccurrences(string s, string part) {
+        // while(s.find(part) != string::npos){
+        //     s.erase(s.find(part) , part.length());
+        // }
+        // return s ; 
+        
+       return  Recursive_Solution(s,part);
         
     }
 };
