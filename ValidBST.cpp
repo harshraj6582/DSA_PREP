fc7d40fde1c8a520c5fe7bcae98ec0b50ac91307
harshraj6582 <harshraj211430@gmail.com>
2024-08-20 16:36:42 +0530

    LCA of BST

--- ValidBST.cpp
+++ ValidBST.cpp
@@ -1,35 +1,35 @@
-bool solve(TreeNode* root , long long int lowerBound , long long int upperBound){
-    // Base Condition 
-    if(root == NULL)
-    {
-        return true ;
-    }
+// bool solve(TreeNode* root , long long int lowerBound , long long int upperBound){
+//     // Base Condition 
+//     if(root == NULL)
+//     {
+//         return true ;
+//     }
 
-    bool cond1 = (root->val > lowerBound);
-    bool cond2 = (root->val < upperBound);
+//     bool cond1 = (root->val > lowerBound);
+//     bool cond2 = (root->val < upperBound);
 
-    bool leftAns = solve(root->left , lowerBound , root->val);
-    bool rightAns = solve(root->right , root->val , upperBound);
+//     bool leftAns = solve(root->left , lowerBound , root->val);
+//     bool rightAns = solve(root->right , root->val , upperBound);
 
-    if(cond1 && cond2 && leftAns && rightAns){
-        return true ;
-    }
-    else{
-        return false ;
-    }
-}
-
-
+//     if(cond1 && cond2 && leftAns && rightAns){
+//         return true ;
+//     }
+//     else{
+//         return false ;
+//     }
+// }
 
 
 
 
-bool isvalidBST(TreeNode* root)
-{
-    long long int lowerBound = -2147483647 ;
-    long long int upperBound = 2147483647 ;
 
-    bool ans = solve(root , lowerBound , upperBound);
 
-    return ans ; 
-}
+// bool isvalidBST(TreeNode* root)
+// {
+//     long long int lowerBound = -2147483647 ;
+//     long long int upperBound = 2147483647 ;
+
+//     bool ans = solve(root , lowerBound , upperBound);
+
+//     return ans ; 
+// }
