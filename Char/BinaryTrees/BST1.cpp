bf5ac1201f9184451970e497a013c51507de026a
harshraj6582 <harshraj211430@gmail.com>
2024-08-22 15:29:55 +0530

    Seach in BST

--- Char/BinaryTrees/BST1.cpp
+++ Char/BinaryTrees/BST1.cpp
@@ -81,6 +81,106 @@ void PreOrder(Node* root)
 
 }
 
+void LevelOrder(Node* root){
+    // Level ORder Traversal of Binary Search Tree Using the 
+    // Queue 
+    queue<Node*> q ; 
+    q.push(root);
+    q.push(NULL);
+
+    // As Queue Contains some sort of data so there are elements 
+    while(!q.empty()){
+        // This Loop will Return until there are elements in the Queue 
+        Node* temp = q.front() ;
+        q.pop() ; 
+        // So basically Each element will be insert 
+        if(temp == NULL){
+            cout<<endl;
+            // This is the condtion for NULL so we need to again check if there are 
+            // elements in the queue so We can insert the NULL parameter 
+            if(q.empty()){
+                break;
+            }
+            else{
+                q.push(NULL);
+                
+            }
+        }
+        else{
+            // This is the Condtion in which there are elements of the Node 
+            // SO we need to print out the elements 
+            cout<<temp->data<<" ";
+            // Now we have to insert the the Left and Right Child of the Elements 
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+Node* MinValue(Node* root)
+{
+    if(root == NULL){
+        return NULL ; 
+    }
+
+    // This is the Base case for checking if the node is Empty or not 
+    if(root -> left == NULL){
+        return root ; 
+    }
+    return MinValue(root->left);
+}
+
+Node* MaxValue(Node* root){
+    // Check for the Condition if the Root is being NULL 
+    if(root == NULL){
+        return NULL ; 
+    }
+    Node* temp = root ; 
+    while(temp->right != NULL){
+        temp = temp->right ;
+    }
+    return temp ; 
+}
+
+Node* MaxValue2(Node* root){
+    // Check for the Base Case 
+    if(root == NULL){
+        return NULL ; 
+        // This is the case in which the Root is being the NULL one 
+
+    }
+    if(root -> right == NULL){
+        return root ;
+    }
+    return MaxValue2(root->right);
+}
+
+bool SearchBST(Node* root  , int target){
+    // This is basically used to check the COnsition of the Empty Ome 
+    if(root == NULL){
+        return false ; 
+    }
+    // Now check for the Condition if the Root one is the taret
+    if(root->data == target){
+        return true ;
+    }
+    bool la = false ; 
+    bool ra = false ; 
+
+    if(target > root->data){
+        ra = SearchBST(root->right , target);
+    }
+    else{
+        la = SearchBST(root->left , target);
+    }
+
+    // IF any of the Answer is True then Return it 
+    return la|| ra ; 
+}
 
 int main(){
 
@@ -89,6 +189,11 @@ int main(){
     insertBST(root);
     // Basically this is used for the Insertion in the BST 
     PreOrder(root);
+    LevelOrder(root);
+
+    // Now Considering the Minimum value of the Tree which would be the left most oart of the Tree 
+   Node* min =  MinValue(root);
+   cout<<min->data; 
 
 
     return 0 ;
