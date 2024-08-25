7c7f0dd0928b292a31c847797fa25142a4f24511
harshraj6582 <harshraj211430@gmail.com>
2024-08-25 17:22:51 +0530

    Heap Lecture 1

--- Char/BinaryTrees/TwoSum.cpp
+++ Char/BinaryTrees/TwoSum.cpp
@@ -1,48 +1,48 @@
-class Solution {
-    public : 
-    // This is just the case in which the following things has to be done 
-    void storeInorder(TreeNode* root , vector<int> &inorder)
-    {
-        // The Following base case is when the following 
-        if(root == NULL)
-        {
-            return ; 
-        }
-        // LNR 
-        storeInorder(root->left  , inorder);
-        // Now Basically perform the Operation 
-        inorder.push_back(root->val);
-        // R 
-        storeInorder(root->right , inorder);
-    }
+// class Solution {
+//     public : 
+//     // This is just the case in which the following things has to be done 
+//     void storeInorder(TreeNode* root , vector<int> &inorder)
+//     {
+//         // The Following base case is when the following 
+//         if(root == NULL)
+//         {
+//             return ; 
+//         }
+//         // LNR   
+//         storeInorder(root->left  , inorder);
+//         // Now Basically perform the Operation 
+//         inorder.push_back(root->val);
+//         // R 
+//         storeInorder(root->right , inorder);
+//     }
 
-    bool findTarget(TreeNode* root , int k )
-    {
-        vector<int> inorder ; 
-        storeInorder(root , inorder);
+//     bool findTarget(TreeNode* root , int k )
+//     {
+//         vector<int> inorder ; 
+//         storeInorder(root , inorder);
 
-        // Find the Target using the 2 Pointer Approach 
-        int start =  ;
-        int end = inorder.size() - 1 ; 
+//         // Find the Target using the 2 Pointer Approach 
+//         int start =  ;
+//         int end = inorder.size() - 1 ; 
 
-        while(start < end)
-        {
-            // This is basically the case we just have sorted Array 
-            int sum = inorder[start] + inorder[end];
-            // This is case in which the target has been achieved 
-            if(sum == k )
-            {
-                return true ; 
-            }
-            else if(sum > k )
-            {
-                end-- ; 
-            }
-            else{
-                start++;
-            }
-        }
-        return true ; 
-        }
-    }
-}
+//         while(start < end)
+//         {
+//             // This is basically the case we just have sorted Array 
+//             int sum = inorder[start] + inorder[end];
+//             // This is case in which the target has been achieved 
+//             if(sum == k )
+//             {
+//                 return true ; 
+//             }
+//             else if(sum > k )
+//             {
+//                 end-- ; 
+//             }
+//             else{
+//                 start++;
+//             }
+//         }
+//         return true ; 
+//         }
+//     }
+// }
