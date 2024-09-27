bcaf93cf299831d78a865ad47f62bd35ed115e8c
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-09-27 22:30:45 +0530

    Time: 20 ms (60.64%), Space: 27.7 MB (51.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,23 +2,34 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) 
     {
-        // We are just using the cocnept of Extra Space in so we are just creating an extra array and them swapping the element
-        // Edge Cases is when the size of  array is 0 or the K == 0 
+        // // We are just using the cocnept of Extra Space in so we are just creating an extra array and them swapping the element
+        // // Edge Cases is when the size of  array is 0 or the K == 0 
+        // int size = nums.size();
+        // if((k == 0) || (size == 0) ){
+        //     return ; // In this case we are just returning thr original array
+        // }
+
+        // // Make the copys of the nums vector 
+        // vector<int> numsCopy(size);
+        // // Make the copy of the Array Elements 
+        // for(int i  = 0 ; i < size ; i++){
+        //     numsCopy[i] = nums[i];
+        // }
+
+        // // Rotate the Elements 
+        // for(int i = 0 ; i < size ; i++){
+        //     nums[(i+k) % size] = numsCopy[i];
+        // }
+
+        // Now we are not using the extra Space 
+        // So we are just reversing the elements in such order that the we end up getting the number 
         int size = nums.size();
-        if((k == 0) || (size == 0) ){
-            return ; // In this case we are just returning thr original array
-        }
+        k = k % size ;
+        reverse(nums.begin() , nums.begin() + (nums.size() - k));
 
-        // Make the copys of the nums vector 
-        vector<int> numsCopy(size);
-        // Make the copy of the Array Elements 
-        for(int i  = 0 ; i < size ; i++){
-            numsCopy[i] = nums[i];
-        }
+        reverse(nums.begin() + (nums.size() -  k) , nums.end() );
 
-        // Rotate the Elements 
-        for(int i = 0 ; i < size ; i++){
-            nums[(i+k) % size] = numsCopy[i];
-        }
+        reverse(nums.begin() , nums.end()); 
+
     }
 };
