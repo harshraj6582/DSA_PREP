6f82f389362f8ac9d949497f9d65fb90ad0d5ce1
harshraj211430 <harshraj211430@gmail.com>
2024-09-28 17:52:26 +0530

    Searching in a Sorted Array

---
+++ Char/BinaryTrees/Sorted_Array.cpp
@@ -0,0 +1,29 @@
+    // int searchInSorted(int arr[], int N, int K) {
+
+    //    // If the Array is being Sorted then use the Method o f Binary Search in this 
+    //    int start = 0 ;
+    //    int size = N ;
+    //    int end = end - 1 ; 
+    //    int mid = start + (end - start ) / 2 ;
+    //    int flag = 0 ;
+       
+       
+       
+    //    while(start <= end){
+    //        if(arr[mid] == K ){
+    //            return 1 ;
+    //        }
+    //        else if( arr[mid] > K){
+               
+    //            end = mid - 1 ;
+    //        }
+    //        else {
+    //            start = mid + 1 ;
+    //        }
+           
+    //        mid = start + (end - start)/ 2 ; 
+           
+    //    }
+       
+    //    return -1 ; 
+    // }
