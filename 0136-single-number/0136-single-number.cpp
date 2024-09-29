2b404e9f1bc93a9be9a06cd5e3c1099a7551f6c4
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-09-29 19:16:03 +0530

    Time: 11 ms (82.12%), Space: 19.5 MB (67.81%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,18 +2,25 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         // We can store all of them in Unordered Map and then check the frequency of each of the digit 
-        unordered_map<int, int > mp ;
-        for(int num : nums ){
-            mp[num]++;
-        }
+        // unordered_map<int, int > mp ;
+        // for(int num : nums ){
+        //     mp[num]++;
+        // }
 
-        for ( auto& pair : mp){
-            if(pair.second == 1 ){
-                return pair.first ; 
-            }
-        }
+        // for ( auto& pair : mp){
+        //     if(pair.second == 1 ){
+        //         return pair.first ; 
+        //     }
+        // }
 
-        return -1 ; 
+        // return -1 ; 
+
+        int ans = 0 ;
+        for(auto x : nums){
+            ans^=x ;
+
+        }
+        return ans ; 
         
     }
 };
