f2728d3fcdfd3d149dc03d296f2761b6adbf2815
harshraj211430 <harshraj211430@gmail.com>
2024-09-29 16:56:58 +0530

    Find the Maximum Number of Ones

---
+++ Char/BinaryTrees/Max_No_Zeroes.cpp
@@ -0,0 +1,16 @@
+// int findMaxOnes(vector<int>  & nums){
+//     int count = 0 ;
+//     int maxi = 0 ;
+//     for(int i = 0 ; i < nums.size() ; i++){
+//         if(nums[i] == 1){
+//             count ++ ;
+//         }
+//         else{
+//             count = 0 ;
+//         }
+
+//         maxi = max(maxi , count );
+//     }
+
+//     return maxi ; 
+// }
