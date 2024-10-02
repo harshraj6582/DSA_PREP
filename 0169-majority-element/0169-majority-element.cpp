e9106914f6bf5471a8523124107f3dac88dccdf0
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-10-02 18:29:53 +0530

    Time: 16 ms (67.44%), Space: 26.8 MB (98.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       unordered_map<int , int > mp ; 
-       for(int i = 0 ; i < nums.size() ; i++){
-        mp[nums[i]] ++;
-       }
+    //    unordered_map<int , int > mp ; 
+    //    for(int i = 0 ; i < nums.size() ; i++){
+    //     mp[nums[i]] ++;
+    //    }
 
-       int count = nums.size() / 2 ;
-       for(auto& pair : mp){
-        if(pair.second > count){
-            return pair.first;
+    //    int count = nums.size() / 2 ;
+    //    for(auto& pair : mp){
+    //     if(pair.second > count){
+    //         return pair.first;
+    //     }
+    //    }
+
+    //    return -1 ; 
+
+    // We are just applying the Moore Voting Algorithm
+    int count = 0 ;
+    int element = nums[0];
+    for(int i = 0 ; i < nums.size() ; i++){
+        if(count == 0){
+            count = 1 ;
+            element = nums[i];
         }
-       }
-
-       return -1 ; 
+        else if (element == nums[i]){
+            count ++ ;
+        }
+        else{
+            count -- ;
+        }
+    }
+    return element ; 
         
     }
 };
