251f136a90174c5c9468c9cf5f2315cee74bd889
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-10-06 17:56:08 +0530

    Time: 0 ms (100%), Space: 8.8 MB (99.43%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -2,33 +2,56 @@ class Solution {
 public:
 
 
-    void recur(vector<int> &ds , vector<int> &nums , vector<vector<int>> & ans , int freq[]){
+    void recur(int index , vector<int> & nums , vector<vector<int>> &ans){
 
-        // Base Condtion of Pushing the element into an Vector
-        if(ds.size() == nums.size()){
-            ans.push_back(ds);
+
+        if(index == nums.size()){
+            ans.push_back(nums);
             return ; 
         }
 
-        for(int i = 0 ; i < nums.size() ; i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i] = 1 ;
-                recur(ds , nums , ans , freq);
-                freq[i] = 0 ;
-                ds.pop_back();
-            }
+        for(int i = index ; i < nums.size() ; i++){
+            swap(nums[index] , nums[i]);
+            recur(index+1 , nums , ans);
+            swap(nums[index] , nums[i]);
         }
+
+
+
+
+
+
+
+        // // Base Condtion of Pushing the element into an Vector
+        // if(ds.size() == nums.size()){
+        //     ans.push_back(ds);
+        //     return ; 
+        // }
+
+        // for(int i = 0 ; i < nums.size() ; i++){
+        //     if(!freq[i]){
+        //         ds.push_back(nums[i]);
+        //         freq[i] = 1 ;
+        //         recur(ds , nums , ans , freq);
+        //         freq[i] = 0 ;
+        //         ds.pop_back();
+        //     }
+        // }
     }
 
     vector<vector<int>> permute(vector<int>& nums) {
         
+        // vector<vector<int>> ans ; 
+        // vector<int> ds ; 
+        // int freq[nums.size()]  ;
+        // for(int i = 0 ; i < nums.size() ; i++) freq[i] = 0 ;
+        // recur(ds , nums , ans , freq);
+        // return ans ;  
+
+
         vector<vector<int>> ans ; 
-        vector<int> ds ; 
-        int freq[nums.size()]  ;
-        for(int i = 0 ; i < nums.size() ; i++) freq[i] = 0 ;
-        recur(ds , nums , ans , freq);
-        return ans ;  
+        recur(0 , nums , ans);
+        return ans ; 
 
     }
 };
