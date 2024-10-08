965c77877cb5f3132d9ec7aac5bb29046bb86662
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-10-08 15:46:37 +0530

    Time: 54 ms (94.05%), Space: 45.4 MB (45.53%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,27 +1,28 @@
+
+
 class Solution {
 public:
     int subarraySum(vector<int>& arr, int k) {
         int n = arr.size();
+        unordered_map<int, int> mp;
+        int curSum = 0;
         int count = 0;
 
-        for(int i = 0 ; i < n ; i++){
-             int sum = 0 ;
-            for(int j = i ; j < n ; j++){
-                // Calculate the Sum of the Sub Array 
-               
+        // Initialize the map with zero sum to handle cases when a subarray starts from index 0
+        mp[0] = 1;
 
-                
-                    sum += arr[j];
-                
+        for (int i = 0; i < n; i++) {
+            curSum += arr[i];
 
-                if(sum == k){
-                    count++;
-                }
+            // Check if there exists a subarray sum that equals k
+            if (mp.find(curSum - k) != mp.end()) {
+                count += mp[curSum - k];
             }
 
+            // Store the cumulative sum in the map
+            mp[curSum]++;
         }
-         return count;
 
-        
+        return count;
     }
 };
