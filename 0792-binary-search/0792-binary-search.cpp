4276803626ab8b8d69920f5ad0c7658add5b7ed2
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-10-09 16:03:25 +0530

    Time: 37 ms (5.72%), Space: 30.2 MB (73.2%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,29 +1,48 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int start = 0 ;
-        int end = n -1 ;
-        int mid = start + (end - start)/2;
+        // int n = nums.size();
+        // int start = 0 ;
+        // int end = n -1 ;
+        // int mid = start + (end - start)/2;
 
-        while(start <= end){
-            if(nums[mid] == target){
-                return mid ; 
-            }
-            else if (nums[mid] > target){
-                end = mid  - 1 ;
-            }
-            else{
-                start = mid + 1 ;
+        // while(start <= end){
+        //     if(nums[mid] == target){
+        //         return mid ; 
+        //     }
+        //     else if (nums[mid] > target){
+        //         end = mid  - 1 ;
+        //     }
+        //     else{
+        //         start = mid + 1 ;
                 
-            }
-            mid = start + (end - start )/2 ;
-        }
+        //     }
+        //     mid = start + (end - start )/2 ;
+        // }
 
 
 
-        return -1 ; 
+        // return -1 ; 
+
+
+    ///////////////////////////////////////////////////////This is Recursive Approach///////////////////////////////////////////////////////////////
+    return binarySearch(nums , 0 , nums.size() - 1 , target);
+
+
     }
+
+
+int binarySearch(vector<int>& nums, int low, int high, int target) {
+
+    if (low > high) return -1; 
+
+    
+    int mid = (low + high) / 2;
+    if (nums[mid] == target) return mid;
+    else if (target > nums[mid])
+        return binarySearch(nums, mid + 1, high, target);
+    return binarySearch(nums, low, mid - 1, target);
+}
 
 
 
