cd6b141756b970f4558c731cffd8298589dcbe0a
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-10-10 17:18:56 +0530

    Time: 15 ms (84.49%), Space: 24.9 MB (62.91%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,20 +1,43 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
+    int singleNonDuplicate(vector<int>& arr) {
         // There is single element that appears only once we have to find out that element 
         // Since the Array is being sorted we have to use the concept of Binary Search
 
-        unordered_map<int,int> mp ; 
-        for(int i = 0 ; i < nums.size() ; i++){
-            mp[nums[i]]++;
-        }
+        int n = arr.size(); // Size of the Array 
 
-        for(auto it : mp){
-            if(it.second == 1 ){
-                return it.first ; 
+        // Edge Cases :
+        if (n == 1 ) return arr[0];
+        if (arr[0] != arr[1] ) return arr[0];
+        if (arr[n-1] != arr[n-2]) return arr[n-1];
+
+        int low = 1  , high = n - 2 ;
+        while(low <= high){
+            int mid = (low + high) / 2 ;
+
+            // Check for the Condition in which arr[mid] is the single element
+            if(arr[mid] != arr[mid+1] && arr[mid] != arr[mid - 1]){
+                return arr[mid];
+            }
+
+
+            // Now we have to check whether we are on the Left Part of the Answer or the Right part 
+            // of the answer 
+            if((mid % 2 == 1  && arr[mid] == arr[mid - 1 ]) || (mid % 2 == 0 && arr[mid] == arr[mid +1])){
+                // we are on the Left edge of the answer so we have to eliminate then Left part 
+            low = mid + 1 ;
+            }
+
+            else{
+                // We are on the right edge of the answer so we need to eliminate the right 
+                // one first 
+                high = mid - 1 ;
             }
         }
-        return -1 ; 
+        return -1 ;
+
+        
+
 
 
     }
