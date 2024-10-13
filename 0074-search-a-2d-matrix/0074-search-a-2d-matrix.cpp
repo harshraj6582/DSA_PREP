600f4dc293cf8409d4b461b9df335e5081c0c8a6
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2024-10-13 15:18:54 +0530

    Time: 0 ms (100%), Space: 12.3 MB (40.49%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -18,56 +18,56 @@ public:
     
     
 
-//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//         int row = 0;
-//         int col = matrix[0].size() - 1; 
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = 0;
+        int col = matrix[0].size() - 1; 
 
-//         while (row < matrix.size() && col >= 0) { 
-//             if (matrix[row][col] == target) {
-//                 return true;
-//             } else if (matrix[row][col] < target) {
-//                 row++;
-//             } else {
-//                 col--;
-//             }
-//         }
-//         return false;
-//     }
+        while (row < matrix.size() && col >= 0) { 
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
     
     ///////////////////////////////CONVERTING 2D Matrix to 1 D /////////////////////////////////
     
     
     
-     bool searchMatrix(vector<vector<int>>& matrix, int target){
+    //  bool searchMatrix(vector<vector<int>>& matrix, int target){
          
-         int start = 0 ;
-         // Total Number of Elemets in a 2D vector 
-         int rows = matrix.size() ;
-         // Total Number of Rows
-         int col = matrix[0].size();
-         // This will print the number of Coloums 
-         int total = rows * col;
+    //      int start = 0 ;
+    //      // Total Number of Elemets in a 2D vector 
+    //      int rows = matrix.size() ;
+    //      // Total Number of Rows
+    //      int col = matrix[0].size();
+    //      // This will print the number of Coloums 
+    //      int total = rows * col;
          
-         int end = total - 1  ; 
-         int mid = start + (end - start)/2;
+    //      int end = total - 1  ; 
+    //      int mid = start + (end - start)/2;
          
-         while(start<=end){
-             int matrix_row = mid / col ;
-             int matrix_col = mid  % col ;
+    //      while(start<=end){
+    //          int matrix_row = mid / col ;
+    //          int matrix_col = mid  % col ;
              
-             if(matrix[matrix_row][matrix_col]  == target){
-                 return true ;
-             }
-             else if  (matrix[matrix_row][matrix_col]  < target){
-                 start = mid + 1 ;
-             }
-             else {
-                 end = mid - 1;
-             }
-             mid = start + (end - start)/2;
-         }
-          return false ;
-         }
+    //          if(matrix[matrix_row][matrix_col]  == target){
+    //              return true ;
+    //          }
+    //          else if  (matrix[matrix_row][matrix_col]  < target){
+    //              start = mid + 1 ;
+    //          }
+    //          else {
+    //              end = mid - 1;
+    //          }
+    //          mid = start + (end - start)/2;
+    //      }
+    //       return false ;
+    //      }
    
    
      
