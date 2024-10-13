f1ad3c9c0d62ae46f0281f33f7f2623dd30f4a37
harshraj211430 <harshraj211430@gmail.com>
2024-10-13 10:59:51 +0530

    Row with Max 1s

--- Max_One.cpp
+++ Max_One.cpp
@@ -24,3 +24,44 @@
 //         return row_index ; 
 //     }
 // };
+
+
+//     int lowerBound(vector<int> arr, int n, int x) {
+//     int low = 0, high = n - 1;
+//     int ans = n;
+
+//     while (low <= high) {
+//         int mid = (low + high) / 2;
+//         // maybe an answer
+//         if (arr[mid] >= x) {
+//             ans = mid;
+//             //look for smaller index on the left
+//             high = mid - 1;
+//         }
+//         else {
+//             low = mid + 1; // look on the right
+//         }
+//     }
+//     return ans;
+// }
+  
+  
+//     int rowWithMax1s(vector<vector<int> > &arr) {
+//         int n = arr.size();
+//         int m = arr[0].size();
+//         int cnt_max = 0 ;
+//         int index = -1 ; 
+        
+//         // Get the Number of 1's :
+//         for(int i = 0 ; i < n ; i++){
+//             int cnt_ones = m - lowerBound(arr[i] , m , 1 );
+//             if(cnt_ones > cnt_max){
+//                 cnt_max = cnt_ones ; 
+//                 index = i ; 
+//             }
+//         }
+        
+//         return index ; 
+        
+        
+//     }
