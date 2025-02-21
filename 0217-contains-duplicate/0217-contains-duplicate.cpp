70c5acef9961b5653d37f7ee7c82513ec16b802b
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2025-02-21 23:27:27 +0530

    Time: 11 ms (99.15%), Space: 61.1 MB (96.13%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,13 +2,30 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
 
-        unordered_map<int , int> mp ; 
-        for(auto it : nums){
-            mp[it]++;
-            // This will store the frequency of the elemets of the Array 
-        }
-        for(auto it : mp){
-            if(it.second > 1){
+        // unordered_map<int , int> mp ; 
+        // for(auto it : nums){
+        //     mp[it]++;
+        //     // This will store the frequency of the elemets of the Array 
+        // }
+        // for(auto it : mp){
+        //     if(it.second > 1){
+        //         return true ; 
+        //     }
+        // }
+        // return false ; 
+
+        // Now we are using the Brute Force Approach 
+        // for(int i = 0 ; i < nums.size() - 1 ; i++){
+        //     for(int j = i + 1 ; j < nums.size() ; j++){
+        //         if(nums[i] == nums[j]){
+        //             return true ; 
+        //         }
+        //     }
+        // }
+        // return false; 
+        sort(nums.begin() , nums.end());
+        for(int i = 0 ; i < nums.size()  - 1 ; i++){
+            if(nums[i] == nums[i+1]){
                 return true ; 
             }
         }
