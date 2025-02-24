e50c5594f1662a743874f09c4db815dd686ee3e4
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2025-02-24 20:30:04 +0530

    Time: 4 ms (60.96%), Space: 16.1 MB (7.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,16 +3,41 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;  
 
-        for (int i = 0; i < nums.size() - 1 ; i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;  // Return immediately after finding the first pair
-                }
+        // for (int i = 0; i < nums.size() - 1 ; i++) {
+        //     for (int j = i + 1; j < nums.size(); j++) {
+        //         if (nums[i] + nums[j] == target) {
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //             return ans;  // Return immediately after finding the first pair
+        //         }
+        //     }
+        // }
+        
+        // return ans;  // Should never reach here as per problem constraints
+
+
+        /**************************************************************************************///
+        /// This is the Brute Force Approach So we need to find the Optimnal One 
+
+        // The New Approach is to Make a Hash Map and store all the Indices to the elements 
+
+        unordered_map<int , int > mp ; 
+
+        int n = nums.size(); 
+
+        // Build the Hash Table 
+        for(int i = 0 ; i < n ; i++){
+            mp[nums[i]] = i ; 
+        }
+
+        // Find the Complement 
+        for(int i = 0 ; i < n ; i++){
+            int com = target - nums[i];
+            if(mp.count(com) && mp[com] != i){
+                return {i , mp[com]};
             }
         }
-        
-        return ans;  // Should never reach here as per problem constraints
+
+        return {};
     }
 };
