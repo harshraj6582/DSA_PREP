ffad34c97c21ac6fad94dabc2c636148edef2a2e
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2025-02-24 20:08:07 +0530

    Time: 0 ms (100%), Space: 9.7 MB (74.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -15,19 +15,36 @@ public:
         // The Second Approach is using the Map Data Strcture to count the Number of the Elemnts in it 
         **///
 
-        unordered_map<char,int> mp ; 
-        for(char it : s ){
-            mp[it]++;
+        // unordered_map<char,int> mp ; 
+        // for(char it : s ){
+        //     mp[it]++;
+        // }
+        // for(char it : t){
+        //     mp[it]--;
+        // }
+
+        // for(auto it : mp){
+        //     if(it.second != 0){
+        //         return false ; 
+        //     }
+        // }       
+        // return true ;  
+
+
+        int count[26] = {0};
+        // Count the Frequency of each of the charcater 
+        for(char x : s){
+            count[x - 'a' ]++;
         }
-        for(char it : t){
-            mp[it]--;
+        for(char x : t){
+            count[x - 'a']--;
         }
 
-        for(auto it : mp){
-            if(it.second != 0){
-                return false ; 
+        for(int val : count){
+            if(val != 0){
+                return false; 
             }
-        }       
-        return true ;  
+        }
+        return true ; 
     }
 };
