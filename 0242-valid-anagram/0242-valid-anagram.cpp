4466f4183cd9226d2f5f6a4f2b6d80f82d96ad30
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2025-02-24 19:55:14 +0530

    Time: 6 ms (28.24%), Space: 9.7 MB (48.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-//         sort(s.begin() , s.end());
-//         sort(t.begin(), t.end());
-        
-//         // Brute Force Approach 
-//         // Time Complexity for this code is O(nlogn)
-//         // We will compare if both of the string are equal or not 
-//         if(s != t ){
-//             return false ; 
-//         }
-//         else{
-//             return true ; 
-//         }
-        
-        // Using the Technique of the Unorderd Map 
-        unordered_map<char , int> frequency ; 
-        for(char c : s ){
-            frequency[c]++;
-        }
-        for(char c : t){
-            frequency[c]--;
-        }
-        bool allZeros = true;
+        sort(s.begin() , s.end());
+        sort(t.begin() , t.end());
 
-    // Iterate through the map
-    for (const auto& pair : frequency)
-    {
-        if (pair.second != 0)
-        {
-            return false ;
+        if(s.size() != t.size()){
+            return false ; 
         }
-    }
-        return true ;
+        if(s == t) return true ; 
+        return false ; 
         
     }
 };
