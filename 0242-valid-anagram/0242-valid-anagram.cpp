5213c5023b026d09a861afbe83f474a4e6665431
Harsh Raj <114869145+harshraj6582@users.noreply.github.com>
2025-02-24 19:59:53 +0530

    Time: 0 ms (100%), Space: 9.7 MB (74.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin() , s.end());
-        sort(t.begin() , t.end());
+        // sort(s.begin() , s.end());
+        // sort(t.begin() , t.end());
 
-        if(s.size() != t.size()){
-            return false ; 
+        // if(s.size() != t.size()){
+        //     return false ; 
+        // }
+        // if(s == t) return true ; 
+        // return false ; 
+
+        // This is the Brute Force Approach as we are using the inbuilt function so the time complexity is O(NLogN)
+        /*******************************************************************************************************************************************
+        // The Second Approach is using the Map Data Strcture to count the Number of the Elemnts in it 
+        **///
+
+        unordered_map<char,int> mp ; 
+        for(char it : s ){
+            mp[it]++;
         }
-        if(s == t) return true ; 
-        return false ; 
-        
+        for(char it : t){
+            mp[it]--;
+        }
+
+        for(auto it : mp){
+            if(it.second != 0){
+                return false ; 
+            }
+        }       
+        return true ;  
     }
 };
